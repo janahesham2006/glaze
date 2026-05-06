980fa639cc1ff97c3ede705216ebffa0a65aca08
Souhayla Ahmed <souhaylaahmed839@gmail.com>
2026-05-07 01:19:45 +0300

    .

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "GLAZE";
+$conn = mysqli_connect($server, $user, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
